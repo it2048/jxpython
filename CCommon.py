5f5cdff8b81c2e947255b64510ac2ecf755fdea6
jsjscool <jsjscool@163.com>
2015-01-12 22:29:11 +0800

    初始化项目

---
+++ CCommon.py
@@ -0,0 +1,52 @@
+__author__ = 'xfl'
+#-*- coding:utf-8 -*-
+import requests
+import MySQLdb
+import time
+import re
+
+class CCommon:
+    def getHtml(self,url):
+        response = requests.get(url)
+        return response.content
+
+    def getConnect(self):
+        return MySQLdb.connect(host='12345.mysql.rds.aliyuncs.com', user='67894',passwd='1234',db='123',port=6,charset='utf8')
+
+    def exeSql(self,sql):
+        try:
+            tim = time.strftime('%Y%m%d',time.localtime(time.time()))
+            conn = self.getConnect()
+            cur = conn.cursor()
+            cur.execute('SET NAMES utf8')
+            cur.execute(sql)
+            conn.commit()
+            cur.close()
+            conn.close()
+        except MySQLdb.Error,e:
+            print "%s :Mysql Error %d: %s \n" % (tim,e.args[0], e.args[1])
+
+    def querySql(self,sql):
+        try:
+            tim = time.strftime('%Y%m%d',time.localtime(time.time()))
+            conn = self.getConnect()
+            cur = conn.cursor()
+            cur.execute('SET NAMES utf8')
+            cur.execute(sql)
+            rows =cur.fetchall()
+            cur.close()
+            conn.close()
+            return rows
+        except MySQLdb.Error,e:
+            print "%s :Mysql Error %d: %s \n" % (tim,e.args[0], e.args[1])
+
+    def downloadImageFile(self,imgUrl,path):
+        local_filename = str(time.strftime('%H%M%S',time.localtime(time.time())))+imgUrl.split('/')[-1]
+        r = requests.get(imgUrl, stream=True) # here we need to set stream = True parameter
+        with open(path+local_filename,'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+                    f.flush()
+            f.close()
+        return local_filename
